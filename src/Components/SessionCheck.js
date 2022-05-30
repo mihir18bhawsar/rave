6fda8cc2f983172e827c875f5ab2bd8d47ee93dc
mihir18bhawsar <mihir18bhawsar@gmail.com>
2022-05-30 08:34:33 +0530

    changes

--- src/Components/SessionCheck.js
+++ src/Components/SessionCheck.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import history from "../history";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../Actions";
+import jwtDecode from "jwt-decode";
 
 const SessionCheck = () => {
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
@@ -9,7 +10,13 @@ const SessionCheck = () => {
   useEffect(() => {
     const unlisten = history.listen(() => {
       if (isLoggedIn) {
-        !sessionStorage.getItem("token") && dispatch(logout());
+        const token = sessionStorage.getItem("token");
+        if (!token) {
+          dispatch(logout());
+          return;
+        }
+        const exp = Date.now() - jwtDecode(token).iat * 1000;
+        exp > 3600000 && dispatch(logout());
       }
     });
     return () => {
--- src/Reducers/sessionReducer.js
+++ src/Reducers/sessionReducer.js
@@ -1,4 +1,7 @@
-const INITIAL = sessionStorage.getItem("token") ? true : false;
+import jwtDecode from "jwt-decode";
+const token = sessionStorage.getItem("token");
+const exp = token && Date.now() - jwtDecode(token).iat * 1000;
+const INITIAL = token && exp < 3600000 ? true : false;
 
 const reducer = (state = INITIAL, action) => {
   if (action.type === "LOGIN") return true;
