4e30145802c8e550bbcfe6a674ba996243bcc079
mihir18bhawsar <mihir18bhawsar@gmail.com>
2022-06-01 20:26:48 +0530

    change

--- src/Components/StripePay.js
+++ src/Components/StripePay.js
@@ -1,16 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toastMessage } from "../Actions";
 import apiservice from "../Api/apiService";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from "./CheckoutForm";
+
 import { useHistory } from "react-router-dom";
+import Loading from "./Loading";
 const StripePay = () => {
-  const [clientSecret, setClientSecret] = useState();
-  const stripePromise = loadStripe(
-    "pk_test_51KwRvDSEMOu66qSiILeui5ZRo0uKe2adVE5wo9FNdeuhaeyafq5Q8BkRcseEwaBVstbuibozPXbmut2wSE5Uk79d00Yq5X9jNX"
-  );
   const history = useHistory();
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
   const dispatch = useDispatch();
@@ -23,30 +18,19 @@ const StripePay = () => {
           const res = await apiservice.get(
             "/booking/checkout-session/" + history.location?.state?.concert?._id
           );
-          setClientSecret(res.data.client_secret);
+          window.location.href = res.data.session.url;
         } catch (err) {
-          console.log(err);
-          dispatch(
-            toastMessage(0, err.response?.data.message || "unknown error")
-          );
+          dispatch(toastMessage(0, err.message || "unknown error"));
         }
       };
       x();
     }
-  }, [history.location.state]);
-  const options = {
-    // passing the client secret obtained from the server
-    clientSecret,
-  };
+  }, []);
+
   return (
-    clientSecret && (
-      <>
-        {"hello"}
-        <Elements stripe={stripePromise} options={options}>
-          <CheckoutForm />
-        </Elements>
-      </>
-    )
+    <>
+      <Loading />
+    </>
   );
 };
 
