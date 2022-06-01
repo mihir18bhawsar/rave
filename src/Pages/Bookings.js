f3f966a367c4cdbca4f8e6e66330dcd60e376ddf
mihir18bhawsar <mihir18bhawsar@gmail.com>
2022-06-01 22:42:00 +0530

    changes

--- src/Pages/Bookings.js
+++ src/Pages/Bookings.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 import apiservice from "../Api/apiService";
 import Loading from "../Components/Loading";
+import ConcertCard from "../Components/ConcertCard";
 
 const Bookings = () => {
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
   useEffect(() => {
     setLoading(true);
     const x = async () => {
       const bookings = await apiservice.get("/user/me/bookings");
-      console.log(bookings.data, "bookingasd");
+      setData(
+        bookings.data.myBookings.map((b) => b.concertId),
+        "bookingasd"
+      );
       setLoading(false);
     };
     x();
@@ -16,11 +21,17 @@ const Bookings = () => {
   return loading ? (
     <Loading />
   ) : (
-    <div className="w-full h-full p-4">
-      <div className="w-full h-full flex flex-col items-center rounded-3xl bg-slate-600">
-        <div className="9/12 h-full grid grid-cols-4">hey</div>
+    data.length > 0 && (
+      <div className="w-full h-full p-4">
+        <div className="w-full h-full flex flex-col items-center rounded-3xl bg-slate-600">
+          <div className="9/12 h-full grid grid-cols-4">
+            {data.map((c) => (
+              <ConcertCard concert={c} />
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
